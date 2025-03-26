266cf02c0fe29492692e606ae636f588887523d7
Kaviya <115607543+kavvv-official@users.noreply.github.com>
2025-03-26 15:28:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thanks for reaching out! I'll get back to you soon.");
+});
